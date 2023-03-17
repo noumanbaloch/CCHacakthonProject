831134dff56786cc2a214dd1fe360e27eee9a692
Nouman Baloch <nomibaloch775@hotmail.com>
2023-03-17 20:22:21 +0500

    working

---
+++ assets/js/app.js
@@ -0,0 +1,150 @@
+// declare global variable
+let video = null; // video element
+let detector = null; // detector object
+let detections = []; // store detection result
+let videoVisibility = true;
+let detecting = false;
+let speachEnabled = false;
+
+// global HTML element
+const toggleVideoEl = document.getElementById('toggleVideoEl');
+const toggleDetectingEl = document.getElementById('toggleDetectingEl');
+const countEl = document.getElementById('countEl'); 
+const toggleSpeakingEl = document.getElementById('toggleSpeakingEl'); 
+// set cursor to wait until video elment is loaded
+document.body.style.cursor = 'wait';
+
+// The preload() function if existed is called before the setup() function
+function preload() {
+    // create detector object from "cocossd" model
+    detector = ml5.objectDetector('cocossd');
+    console.log('detector object is loaded');
+  }
+  
+  // The setup() function is called once when the program starts.
+  function setup() {
+    // create canvas element with 640 width and 480 height in pixel
+    createCanvas(640, 480);
+    // Creates a new HTML5 <video> element that contains the audio/video feed from a webcam.
+    // The element is separate from the canvas and is displayed by default.
+    video = createCapture(VIDEO);
+    video.size(640, 480);
+    console.log('video element is created');
+    video.elt.addEventListener('loadeddata', function() {
+      // set cursor back to default
+      if (video.elt.readyState >= 2) {
+        document.body.style.cursor = 'default';
+        console.log('video element is ready! Click "Start Detecting" to see the magic!');
+      }
+    });
+  }
+
+  function toggleVideo() {
+    if (!video) return;
+    if (videoVisibility) {
+      video.hide();
+      toggleVideoEl.innerText = 'Show Video';
+    } else {
+      video.show();
+      toggleVideoEl.innerText = 'Hide Video';
+    }
+    videoVisibility = !videoVisibility;
+  }
+  
+  function toggleDetecting() {
+    if (!video || !detector) return;
+    if (!detecting) {
+      detect();
+      toggleDetectingEl.innerText = 'Stop Detecting';
+    } else {
+      toggleDetectingEl.innerText = 'Start Detecting';
+    }
+    detecting = !detecting;
+  }
+
+  function toggleTextToSpeech() {
+    if (!speachEnabled)
+    {
+        speachEnabled = true;
+        toggleSpeakingEl.innerText = 'Text to Speech Enabled';
+    } else {
+        speachEnabled = false;
+        toggleSpeakingEl.innerText = 'Text to Speech Disabled';
+    }
+  }
+
+  function detect() {
+    // instruct "detector" object to start detect object from video element
+    // and "onDetected" function is called when object is detected
+    detector.detect(video, onDetected);
+  }
+  
+  // callback function. it is called when object is detected
+  function onDetected(error, results) {
+    if (error) {
+      console.error(error);
+    }
+    detections = results;
+
+    countEl.innerText = `Number of objects detected: ${detections.length}`; // update count element
+    
+    if(!speachEnabled && 'speechSynthesis' in window)
+    {
+        window.speechSynthesis.cancel();
+    }
+    
+    if (speachEnabled && 'speechSynthesis' in window) {
+        debugger
+        const message = new SpeechSynthesisUtterance(`Number of objects detected: ${detections.length}`);
+        window.speechSynthesis.speak(message);
+    } else {
+        console.log('Sorry, speech synthesis is not supported by your browser.');
+    }
+    
+    // keep detecting object
+    if (detecting) {
+      detect(); 
+    }
+  }
+
+
+  // the draw() function continuously executes until the noLoop() function is called
+function draw() {
+    if (!video || !detecting) return;
+    // draw video frame to canvas and place it at the top-left corner
+    image(video, 0, 0);
+    // draw all detected objects to the canvas
+    for (let i = 0; i < detections.length; i++) {
+      drawResult(detections[i]);
+    }
+  }
+  
+  function drawResult(object) {
+    drawBoundingBox(object);
+    drawLabel(object);
+  }
+  
+  // draw bounding box around the detected object
+  function drawBoundingBox(object) {
+    // Sets the color used to draw lines.
+    stroke('green');
+    // width of the stroke
+    strokeWeight(4);
+    // Disables filling geometry
+    noFill();
+    // draw an rectangle
+    // x and y are the coordinates of upper-left corner, followed by width and height
+    rect(object.x, object.y, object.width, object.height);
+  }
+  
+  // draw label of the detected object (inside the box)
+  function drawLabel(object) {
+    // Disables drawing the stroke
+    noStroke();
+    // sets the color used to fill shapes
+    fill('white');
+    // set font size
+    textSize(24);
+    // draw string to canvas
+    text(object.label, object.x + 10, object.y + 24);
+  }
